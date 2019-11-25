7a32f748f262f356120b515b985b7bbe3dec70ed
HwH <563580920@qq.com>
2019-11-25 11:12:59 +0800

    添加了内容

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,7 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
     <router-view></router-view>
-=======
-<<<<<<< HEAD
-    
-    <router-view></router-view>
-=======
->>>>>>> 50779abf67839f51bb4734348c3193ff5d2e2680
     <Footer></Footer>
->>>>>>> 0b3adc70c8479b2e0b3427e64ae174cde7da7b68
   </div>
 </template>
 
@@ -24,27 +16,17 @@ export default {
 }
 </script>
 
-<<<<<<< HEAD
 <style>
 *{
     padding: 0;
     margin: 0;
 }
-=======
-<style scope>
->>>>>>> 50779abf67839f51bb4734348c3193ff5d2e2680
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-<<<<<<< HEAD
   color: #2c3e50;
   margin-top: 60px;
 
-=======
-  /* text-align: center;
-  color: #2c3e50;
-  margin-top: 60px; */
->>>>>>> 50779abf67839f51bb4734348c3193ff5d2e2680
 }
 </style>
